088a943b2f7cd39dacc8213fa21780f7498d2911
Adam LAAOUANE <adam.laaouane.pro@gmail.com>
2023-12-07 22:59:45 +0100

    Update Pioche.java

--- Citadelles/src/modele/Pioche.java
+++ Citadelles/src/modele/Pioche.java
@@ -1,39 +1,32 @@
 package modele;
 
 import java.util.ArrayList;
-import java.util.Random;
+import java.util.Collections;
 
 public class Pioche {
 	private ArrayList<Quartier> liste;
-	Random generateur;
+
 	public Pioche() {
-		liste = new ArrayList<Quartier>();
+		liste = new ArrayList<>();
 	}
+
 	public Quartier piocher() {
-		Quartier pioche = null;
 		if (liste.isEmpty()) {
-			return pioche;
-		} 
-        else{
-			pioche = liste.get(liste.size()-1);
-			liste.remove(liste.size()-1);
+			return null;
+		} else {
+			return liste.remove(liste.size() - 1);
 		}
-		return pioche;
 	}
+
 	public void ajouter(Quartier nouveau) {
-		liste.add(0, nouveau);
+		liste.add(nouveau); // Ajoute à la fin de la liste pour être pioché en dernier
 	}
+
 	public int nombreElements() {
 		return liste.size();
 	}
+
 	public void melanger() {
-		generateur = new Random();
-		for (int compteur = 0; compteur<nombreElements(); compteur++) {
-			int i = generateur.nextInt(nombreElements());
-			int j = generateur.nextInt(nombreElements());
-			Quartier quartier = liste.get(i);
-			liste.set(i, liste.get(j));
-			liste.set(j, quartier);
-		}
+		Collections.shuffle(liste); // Utilise la méthode shuffle de Collections pour mélanger
 	}
 }
